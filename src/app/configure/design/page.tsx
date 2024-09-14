24a26c3d8fab081a14120dec906f61b32a75ad60
meshJohn <meshjohn1@gmail.com>
2024-09-14 06:35:50 +0300

    first commit

---
+++ src/app/configure/design/page.tsx
@@ -0,0 +1,37 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurate from "./DesignConfigurate";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  // make db call
+  const { id } = searchParams;
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+  const configuration = await db.configuration.findUnique({
+    where: {
+      id,
+    },
+  });
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurate
+      configId={configuration.id}
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
+};
+
+export default Page;
